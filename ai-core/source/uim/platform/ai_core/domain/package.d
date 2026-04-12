158430bd3e0eb731ca6b53afda7e9aa6f73fdb15
UIMSolutions <ui.manufaktur@gmail.com>
2026-04-12 10:06:30 +0200

    Remove ai-core

--- ai-core/source/uim/platform/ai_core/domain/package.d
+++
@@ -1,8 +0,0 @@
-module uim.platform.ai_core.domain;
-
-public {
-  import uim.platform.ai_core.domain.types;
-  import uim.platform.ai_core.domain.entities;
-  import uim.platform.ai_core.domain.ports;
-  import uim.platform.ai_core.domain.services;
-}
--- ai-core/source/uim/platform/ai_core/domain/services/package.d
+++
@@ -1,6 +0,0 @@
-module uim.platform.ai_core.domain.services;
-
-public {
-  import uim.platform.ai_core.domain.services.scenario_validator;
-  import uim.platform.ai_core.domain.services.execution_scheduler;
-}
